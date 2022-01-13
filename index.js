e927933bf3f1af24477d8eb9bc34aa3929658ec2
Luis Cardona <l.cardona@astoundcommerce.com>
2022-01-13 10:12:06 -0500

    creacion de squemas con joi para validacion de datos

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ const app = express();
 
 const { config } = require('./config');
 const moviesApi = require('./routes/movies.js');
+const {
+  logErrors,
+  errorHandler,
+  wrapErrors,
+} = require('./util/middleware/errorHandlers.js');
+
+const notFoundHandler = require('./util/middleware/notFoundHandler.js');
 
 // Body parser
 app.use(express.json());
 
+// router
 moviesApi(app);
+// Catch 404
+app.use(notFoundHandler);
+
+// Error middlewares
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log(`Listening on http://localhost:${config.port}`);
