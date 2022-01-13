b2cc808ee2644878a623f135d89f823aa4d9ef2d
Luis Cardona <l.cardona@astoundcommerce.com>
2022-01-12 23:03:38 -0500

    Remove mock and added instance of mongoDb and mongo Lib

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
-const { config } = require('./config')
+const { config } = require('./config');
 const moviesApi = require('./routes/movies.js');
+
+// Body parser
+app.use(express.json());
 
 moviesApi(app);
 
 app.listen(config.port, () => {
-    console.log(`Listening on http://localhost:${config.port}`);
+  console.log(`Listening on http://localhost:${config.port}`);
 });
